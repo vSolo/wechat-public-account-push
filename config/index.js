c075d641f889c1c03aea7211d403f5f313236b9a
vSolo <vSolo@users.noreply.github.com>
2022-08-26 22:36:27 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wx534772a87ecc7ddb",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "40eb4678375dd562f1c5ce569fc02c25",
     // 模板消息id
     templateId: "",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: [],
+    user: [oE5ms6QiXOaz9yJsTLntlFe7nsGg],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "山东",
     // 所在城市
-    city: "惠州",
+    city: "威海",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
       {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
